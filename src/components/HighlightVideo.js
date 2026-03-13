1ca3306dc2f866e11a80c386362ec11c59c14eda
Lucas <lucasoliveiraguedes00@gmail.com>
2026-03-13 10:11:28 -0300

    Carregando banco de vídeos

--- src/components/HighlightVideo.js
+++ src/components/HighlightVideo.js
@@ -5,37 +5,22 @@ import style from './HighlightVideo.module.css'
 import { getRandomItem, objectToArray } from '@/helpers/helpers'
 import Link from 'next/link'
 
-const videos = {
-    "d40wvJmPD9k": {
-      "title": "Mais do que resistência, é sobre responsabilidade | Pr Telmo Martinello",
-      "description": "Um alerta para que o homem permaneça vigilante, firme na fé e responsável pelo cuidado espiritual de sua casa.",
-      "tags": [ "Vídeos", "Documentários e Informativos" ]
-    },
-
-    "BqXmSzyl67s": {
-        "title": "O Mistério da Origem da Vida",
-        "description": "Descubra os mistérios por trás do surgimento da vida no planeta Terra. Este documentário explora teorias científicas e ideias intrigantes que desafiam nossa compreensão do universo. Uma jornada emocionante pelo desconhecido.",
-        "tags": ["Vídeos", "Documentários e Informativos"]
-    },
-    "Fxx52dJGBA4": {
-      "title": "Leandro Marques - Sansão sem noção",
-      "description": "Álbum: EU TENHO GRAÇA Lançamento: Graça Music 2015 ® Letra e Música: Leandro Marques",
-      "tags": [ "Músicas", "Documentários e Informativos" ]
-    },
-    "0Xx6UW2IOSw": {
-      "title": "Hits do TikTok 2024",
-      "description": "Confira os maiores sucessos do TikTok de 2024. Uma seleção com músicas que não saem da sua cabeça e dominam as tendências.",
-      "tags": ["Músicas", "Populares Nacionais"]
-    },
-}
-
 export default function HighlightVideo({ tags }) {
     const [ video, setVideo ] = useState(null)
 
     useEffect(() => {
-        const videosList = objectToArray(videos)
-            .filter(video => tags == undefined || video.tags.includes(tags))
-        setVideo(getRandomItem(videosList))
+        async function fetchVideos() {
+            const response = await fetch('/database.json', {
+                next: {
+                    revalidate: 60
+                }
+            })
+            const videos = await response.json()
+            const videosList = objectToArray(videos)
+                .filter(video => tags == undefined || video.tags.includes(tags))
+            setVideo(getRandomItem(videosList))
+        }
+        fetchVideos()
     }, [])
 
     return (
