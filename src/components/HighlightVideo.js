1f98c8220c746103f0793ee66aef7fed93dba5bc
Lucas <lucasoliveiraguedes00@gmail.com>
2026-03-15 06:24:25 -0300

    Navegação por rotas dinamicamente

--- src/components/HighlightVideo.js
+++ src/components/HighlightVideo.js
@@ -1,21 +1,29 @@
 "use client"
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import style from './HighlightVideo.module.css'
 import { getRandomItem, getVideos, objectToArray } from '@/helpers/helpers'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function HighlightVideo({ tag }) {
+    const router = useRouter()
     const [ video, setVideo ] = useState(null)
+    const videosList = useRef()
 
     useEffect(() => {
         async function fetchVideos() {
-            const videosList = await getVideos(tag)
-            setVideo(getRandomItem(videosList))
+            videosList.current = await getVideos(tag)
+            setVideo(getRandomItem(videosList.current))
         }
         fetchVideos()
     }, [])
 
+    function onLuckyClick(event) {
+        const video = getRandomItem(videosList.current)
+        router.push(`/player/${video.key}`)
+        event.preventDefault()
+    }
     return (
         video && (
             <div className={style.video}>
@@ -25,6 +33,7 @@ export default function HighlightVideo({ tag }) {
                     <h2 className={style.title}>{video.title}</h2>
                     <p className={style.description}>{video.description}</p>
                     <Link href={`/player/${video.key}`} className={style.play}>Assistir</Link>
+                    <Link href='#' onClick={onLuckyClick} className={style.lucky}>Estou com Sorte</Link>
                 </div>
             </div>
         )
